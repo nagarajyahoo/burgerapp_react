50f7fcddb4fdc9dde7627af1c50fb48e7923897e
nagarajmr <nagaraj.mr27@yahoo.co.in>
2018-09-28 21:46:23 +0100

    nmr: tooltip

--- src/containers/signin/SignIn.js
+++ src/containers/signin/SignIn.js
@@ -1,23 +1,57 @@
 import React, {Component} from 'react';
-import Button from '../../ui/button/Button';
 import classes from './SignIn.css'
+import ReactTooltip from 'react-tooltip'
 
 class SignIn extends Component {
     state = {
-        email: {},
-        password: {}
+        email: {
+            value: '',
+            config: {
+                maxLength: 9,
+                minLength: 2
+            }
+        },
+        password: {
+            value: '',
+            config: {
+                maxLength: 9,
+                minLength: 2
+            }
+        }
     };
 
     render() {
         return (
-            <div className={classes.SignIn}>
-                <form>
-                    <input type='email' placeholder='Your Email'/>
-                    <input type='password' placeholder='Password'/>
-                </form>
-                <button type="button" onClick={() => alert('hello')}>Sign In</button>
-            </div>
+                <div className={classes.SignIn}>
+                    <form>
+                        <input type='email'
+                               onChange={(event) => this.validateAndSetVal(event, 'email')}
+                               placeholder='Your Email'
+                               data-tip="Please enter email"
+                               value={this.state.email.value}
+                        />
+                        <input type='password'
+                               onChange={(event) => this.validateAndSetVal(event, 'password')}
+                               placeholder='Password'
+                               value={this.state.password.value}
+                               data-tip="Please enter password"
+                        />
+                    </form>
+                    <button type="submit" onClick={() => this.login()}>Sign In</button>
+                    <ReactTooltip/>
+                </div>
+
         );
+    }
+
+    validateAndSetVal = (event, type) => {
+        this.setState({
+            [type]: event.target.value
+        });
+    }
+
+    login = () => {
+        alert(this.state.email)
     }
 }
 
